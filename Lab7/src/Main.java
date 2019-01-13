291047ba743df12e60ec9951b785bc2e38a85868
Clarissa La Rue <NastenaKorobova98@mail.ru>
2019-01-14 03:12:34 +0700

    finish lab7

---
+++ Lab7/src/Main.java
@@ -0,0 +1,13 @@
+
+import PortForvarder.PortForvarder;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]){
+        try {
+            new PortForvarder.PortForvarder(args).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
